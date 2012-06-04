b5ea32e79e06da3c5562f83481dc00b4a3bb00db
szantog <szantog@610310.no-reply.drupal.org>
2012-06-04 17:38:34 +0200

    Change donate forms redirect to the donate page

--- panparks_donate.js
+++ panparks_donate.js
@@ -8,16 +8,16 @@
   Drupal.behaviors.panparks_donate = {
     attach: function (context, settings) {
       $("input:radio[name='amountSelector']", context).click(function () {
-        $('#ammount-1').val($(this).val());
+        $("input[name='amount']").val($(this).val());
         //alert($(this).val());
      });
 
-      $("#edit-uniqueamount", context).focus(function () {
+      $("input[name='uniqueAmount']", context).focus(function () {
         $("input:radio[name='amountSelector']").attr("checked" , false );
      });
 
-      $("#edit-uniqueamount", context).focusout(function () {
-        $('#ammount-1').val($(this).val());
+      $("input[name='uniqueAmount']", context).focusout(function () {
+        $("input[name='amount']").val($(this).val());
      });
 
       $("#ws_submit", context).click(function () {
@@ -28,7 +28,7 @@
      });
 
       $("#panparks-donate-form-support-documentary input:radio[name='amountSelector']", context).click(function () {
-        $('#ammount-2').val($(this).val());
+        $("input[name='amount']").val($(this).val());
         //alert($(this).val());
      });
     }
